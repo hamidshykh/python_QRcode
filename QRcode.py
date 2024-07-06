e137255b35d09c1a474ffdc035df991ceca4176b
Hamid Shaikh <128905142+hamidshykh@users.noreply.github.com>
2024-07-06 21:35:19 +0500

    qrCode
    code completed!

---
+++ QRcode.py
@@ -0,0 +1,10 @@
+import qrcode 
+from PIL import Image
+
+qr = qrcode.QRCode(version=1,
+                   error_correction=qrcode.constants.ERROR_CORRECT_H,
+                   box_size = 10 , border = 4)
+qr.add_data("https://www.wscubetech.com/")
+qr.make(fit = True)
+img = qr.make_image(fill_color=(255, 0, 0), back_color=(0, 0, 255))
+img.save("wscube_qrcode.png")
