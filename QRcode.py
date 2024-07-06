b21503024f4c72fc1852b009a4ece3991f74e679
Hamid Shaikh <128905142+hamidshykh@users.noreply.github.com>
2024-07-06 21:37:36 +0500

    recorrect

--- QRcode.py
+++ QRcode.py
@@ -4,7 +4,7 @@ from PIL import Image
 qr = qrcode.QRCode(version=1,
                    error_correction=qrcode.constants.ERROR_CORRECT_H,
                    box_size = 10 , border = 4)
-qr.add_data("https://www.wscubetech.com/")
+qr.add_data("https://www.google.com/")
 qr.make(fit = True)
 img = qr.make_image(fill_color=(255, 0, 0), back_color=(0, 0, 255))
-img.save("wscube_qrcode.png")
+img.save("google_qrcode.png")
